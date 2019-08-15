32c35e8df1ebb8abe64545bb8ec8be05a22f57b3
Joshua Rogan <Joshua.Rogan@hmhco.com>
2019-08-15 09:12:48 +0100

    removed unused code and added some spacing

--- panel.js
+++ panel.js
@@ -1,4 +1,4 @@
-// Keep a record of content types and their size in bytes.
+// Keep a record of the caliper track events
 let trackEvents = [];
 
 // HTML element to output our data.
@@ -15,19 +15,22 @@ const render = () => {
 
 // When a network request has finished this function will be called.
 chrome.devtools.network.onRequestFinished.addListener(request => {
-  const response = request.response;
-  const queryStrings = request.queryString;
   let curURL = String(request.request.url);
-  if (curURL == "https://cert.hmhco.com/api/eventservice/v1/caliper/events") {
+
+  if (curURL.endsWith("api/eventservice/v1/caliper/events")) {
     console.log("CALIPER EVENTS: " + curURL);
     console.log(request.request.postData.text);
+
     let splitText = request.request.postData.text.split(",");
     console.log(splitText);
+
     let matches = splitText.filter(s => s.startsWith('\"extensions\":{\"viewType\":'));
     console.log(matches);
+
     let extracted = matches[0].split('\"extensions\":{\"viewType\":\"');
     extracted = extracted[1].slice(0, -1);
     console.log(extracted);
+
     trackEvents.push(extracted);
     console.log(trackEvents);
     render();
